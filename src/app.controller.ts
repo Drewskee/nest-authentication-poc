8daaee340b79db3a8fa7781f68f37f5e58c29f21
Drew Stephenson <andrewdevelops@gmail.com>
2023-02-23 23:22:33 -0500

    jwt-auth gaurd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,21 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.gaurd';
 import { LocalAuthGuard } from './auth/local-auth.gaurd';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private authService: AuthService) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
