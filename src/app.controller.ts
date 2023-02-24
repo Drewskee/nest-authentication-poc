4a5fab5db2be282e8dde23a6e3c91a92a30a36e5
Drew Stephenson <andrewdevelops@gmail.com>
2023-02-23 23:19:20 -0500

    added jwt and local auth strategy

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.gaurd';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+}
